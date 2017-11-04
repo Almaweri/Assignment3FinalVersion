42621cc0fb530da3f3c776e4ccdd67eb5a3b2451
Maged Almaweri <maged.almaweri@yahoo.com>
2017-11-03 21:20:29 -0400

    Initial commit

---
+++ app/src/main/java/edu/cse523/maged/assignment3finalversion/Product.java
@@ -0,0 +1,44 @@
+package edu.cse523.maged.assignment3finalversion;
+
+/**
+ * Created by DP on 10/24/2017.
+ */
+
+public class Product {
+
+   private String name = null;
+   private String price = null;
+   private int image = -1;
+
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getPrice() {
+      return price;
+   }
+
+   public void setPrice(String price) {
+      this.price = price;
+   }
+
+   public int getImage() {
+      return image;
+   }
+
+   public void setImage(int image) {
+      this.image = image;
+   }
+
+   public Product(String name, String price, int image) {
+
+      this.name = name;
+      this.price = price;
+      this.image = image;
+   }
+}
